bf5970a1843f6fed7418ed10ca1e5559f6a3f9a8
a13-team <dezmondfx1@gmail.com>
2022-11-23 09:45:32 +0000

    +

---
+++ resources/lang/ru/days.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+   'days' => [
+       'monday' => 'Понедельник',
+       'tuesday' => 'Вторник',
+       'wednesday' => 'Среда',
+       'thursday' => 'Четверг',
+       'friday' => 'Пятница',
+       'saturday' => 'Суббота',
+       'sunday' => 'Воскресенье',
+   ],
+];
